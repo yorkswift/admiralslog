5598c6d6161b9af67be46a012ccbbf42b518f3d3
Henry Allsuch <henry@agilepixel.io>
2018-11-02 14:22:33 +0000

    • adding index and module generator

---
+++ Source/generateModuleTypePermutations.php
@@ -0,0 +1,41 @@
+<?php
+
+$module_colours = [
+
+    'yellow'=> "#232344",
+    'green'=> "#232344",
+    'purple'=> "#232344",
+    'blue'=>'#342342',
+    'orange'=>'#342342'
+
+];
+
+
+$module_types = [
+    'A'=>[
+        'name'=>'Accommodation',
+        'colour'=>'yellow'
+    ],
+    'D'=> [
+        'name'=>'Defence',
+        'colour'=>'green'
+    ],
+    'E'=>[
+        'name'=>'Education',
+        'colour'=>'green'
+    ],
+    'I'=> [
+        'name'=>'Infrastructure',
+        'colour'=>'green'
+    ],
+    'R'=> [
+        'name'=>'Recreation',
+        'colour'=>'green'
+    ],
+
+];
+
+function generateModuleTypePermutations(){
+
+
+}
