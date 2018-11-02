6b0eadd082af9ed36e76efbdd79e9fd65cc27981
Henry Allsuch <henry@agilepixel.io>
2018-11-02 20:47:54 +0000

    • generating Permutations

--- Source/generateModuleTypePermutations.php
+++ Source/generateModuleTypePermutations.php
@@ -2,14 +2,13 @@
 
 $module_colours = [
 
-    'yellow'=> '#fdd97a',
-    'green'=> '#9dcb8c',
-    'purple'=> '#a088b8',
-    'blue'=>'#a5ddec',
-    'orange'=>'#f47f52'
+    'yellow' => '#fdd97a',
+    'green' => '#9dcb8c',
+    'purple' => '#a088b8',
+    'blue' => '#a5ddec',
+    'orange' => '#f47f52'
 
 ];
-
 
 $module_types = [
     'A'=>[
@@ -35,7 +34,49 @@ $module_types = [
 
 ];
 
-function generateModuleTypePermutations(){
+function generateModuleTypePermutations($module_types){
 
+    $list = array_keys($module_types);
+    $i = 0; 
+
+   $permutations = genPermutations($list, 4);
+
+    $unique_permutations = [];
+    foreach ($permutations as $permutation) {
+
+        sort($permutation);
+        $key = implode('', $permutation);
+        if(!isset($unique_permutations[$key])){
+            $unique_permutations[$key] = $permutation;
+            echo $key . PHP_EOL;
+            $i++;
+        } 
+        
+    }
+    
+    echo $i . ' permutations' . PHP_EOL;
 
 }
+
+function genPermutations($values,$count=0) {
+    
+    $permCount = pow(count($values), $count);
+    for ($i = 0; $i < $permCount; $i++) {
+        yield getPermutation($values, $count, $i);
+    }
+}
+  
+function getPermutation($values, $count, $index) {
+    
+    $result = [];
+    for($i = 0; $i < $count; $i++) {
+      
+      $pos = $index % count($values);
+  
+      $result[] = $values[$pos];
+      $index = ($index-$pos)/count($values);
+    }
+    return $result;
+}
+
+generateModuleTypePermutations($module_types);
