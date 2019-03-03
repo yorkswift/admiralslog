206f779a305417056d35143041007a026094b1ca
Henry Allsuch <henry@agilepixel.io>
2019-03-03 14:26:16 +0000

    • intro video fixes

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -1,6 +1,29 @@
 
+var fallbackIntroTimer;
+
 introEnded = function(){
     
-    $('.modules').removeClass('hidden');
+    if($('.modules').hasClass('hidden')){
+
+        $('body').addClass('background__lab');
+        $('.modules').removeClass('hidden');
+        clearIntroTimer();
+    }
    
 }
+
+introStarted = function(){
+    clearIntroTimer();
+}
+
+clearIntroTimer = function(){
+    if(fallbackIntroTimer){
+        clearInterval(fallbackIntroTimer);
+    }
+}
+
+$( document ).ready(function() {
+
+    fallbackIntroTimer = setInterval(function(){ introEnded(); }, 7000);
+
+});
